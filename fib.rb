35765318b046424a9ee88affaf17021e99200cf1
Xebros85 <rorban1@live.com>
2024-10-16 18:08:57 -0500

    Completed iterative and recursive fibonacci functions

---
+++ fib.rb
@@ -0,0 +1,44 @@
+# Fibonacci Sequence without recursion
+
+def fibs(num)
+  results = [0, 1, 1]
+
+  return 1 if num <= 2
+  first = 1
+  second = 1
+  
+  until num < 3
+    fib = first + second
+    first = second
+    second = fib
+    results << fib
+    num -= 1
+  end
+  results  
+end
+
+# Recursive Fibonacci Sequence
+def fibs_rec(num, results = [0, 1, 1])  
+  # Base Case
+  return results if num <= 2
+  
+  # Recursive Case
+  next_number = results[-1] + results[-2]
+  results << next_number
+
+  fibs_rec(num - 1, results)
+end
+
+number = 10
+
+puts
+start_time = Time.now
+puts "Non-recursive Fibonacci sequence:"
+p fibs(number)
+puts " \t [#{( ( Time.now - start_time) * 1000.0 ).round(3)}ms]"
+
+puts
+start_time = Time.now
+puts "Recursive Fibonacci sequence:"
+p fibs_rec(number)
+puts " \t [#{( ( Time.now - start_time) * 1000.0 ).round(3)}ms]"
