32cc02ae41d1c6fff639e793d7ad293e6972f237
Xebros85 <rorban1@live.com>
2024-10-20 06:57:39 -0500

    Completed Merge Sort

--- merge.rb
+++ merge.rb
@@ -1,7 +1,46 @@
 # Merge Sort
 
-def merge_sort
-  # Code goes here
+
+def merge_sort(arr)
+  # Empty or Nil
+  return [] if arr.nil? || arr.empty?
+
+  # Base Case:
+  return arr if arr.length <= 1
+
+  middle = arr.length / 2
+  left_half = merge_sort(arr[0...middle])
+  right_half = merge_sort(arr[middle..-1])
+  
+  merge_arr(left_half, right_half)
+ 
+end
+
+def merge_arr(left, right)
+  # Merges the array back together  
+  sorted = []
+
+  until left.empty? && right.empty?
+    if left.empty?
+      sorted.concat(right)
+      break
+    elsif right.empty?
+      sorted.concat(left)
+      break
+    else
+      case left[0] <=> right[0]
+      when -1
+        sorted << left.shift
+      when 0
+        sorted << left.shift
+        sorted << right.shift
+      when 1
+        sorted << right.shift
+      end
+    end
+  end
+
+  sorted
 end
 
 
@@ -9,5 +48,5 @@ end
 arr1 = [3, 2, 1, 13, 8, 5, 0, 1]
 arr2 = [105, 79, 100, 110]
 
-p arr1.sort
-p arr2.sort
+p merge_sort(arr1)
+p merge_sort(arr2)
